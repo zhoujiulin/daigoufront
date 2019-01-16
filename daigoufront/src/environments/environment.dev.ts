89fcbeb7e71d613d0af498909bc3bd7a441a9788
zhoujiulin <zhoujiulin18@gmail.com>
2019-01-16 15:32:07 +0100

    port

--- daigoufront/src/environments/environment.dev.ts
+++ daigoufront/src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   envName: "dev",
-  baseUrl: ""
+  baseUrl: "ec2-35-180-98-2.eu-west-3.compute.amazonaws.com:8081"
 };
--- daigoufront/src/environments/environment.ts
+++ daigoufront/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   envName: "local",
-  baseUrl: "http://localhost:8080"
+  baseUrl: "http://localhost:8081"
 };
 
 /*
